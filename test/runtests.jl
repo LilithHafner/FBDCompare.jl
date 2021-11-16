8fdbc0f047a1250fe1a532e5527d73950d682cc6
Lilith Orion Hafner <60898866+LilithHafner@users.noreply.github.com>
2021-11-16 00:08:54 -0600

    don't test external during CI b.c. numpy is not installed

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,26 @@ using FBDCompare
 using Test
 
 ci = ("CI"=>"true") ∈ ENV
-gui = !ci
 
 @testset "Kronecker" begin
-    if gui
-        Kronecker.plot()
-    else
+    if ci
         Kronecker.generate_data()
-        @test_broken false
+        @test_broken false #=
+https://github.com/LilithHafner/FBDCompare.jl/runs/4220893477?check_suite_focus=true
+qt.qpa.xcb: could not connect to display
+qt.qpa.plugin: Could not load the Qt platform plugin "xcb" in "" even though it was found.
+This application failed to start because no Qt platform plugin could be initialized. Reinstalling the application may fix this problem.
+
+Available platform plugins are: linuxfb, minimal, offscreen, vnc, xcb.
+
+Aborted (core dumped)
+connect: Connection refused
+GKS: can't connect to GKS socket application
+
+
+signal (11): Segmentation fault
+=#  else
+        Kronecker.plot()
     end
     @test true
 end
@@ -21,6 +33,15 @@ end
     HyperPA.jl_benchmark()
     HyperPA.jl_profile()
     HyperPA.jl_with_io()
-    HyperPA.external()
+    if ci
+        @test_broken false #=
+https://github.com/LilithHafner/FBDCompare.jl/runs/4221035425?check_suite_focus=true
+Traceback (most recent call last):
+  File "hyper_preferential_attachment.py", line 8, in <module>
+    import numpy as np
+ModuleNotFoundError: No module named 'numpy'
+=#  else
+        HyperPA.external()
+    end
     @test true
 end
