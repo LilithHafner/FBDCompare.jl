7f5c3a81ff6171315593324d5e5663d032714711
Lilith Orion Hafner <60898866+LilithHafner@users.noreply.github.com>
2022-01-28 12:44:32 -0600

    add initail DCHSBM (wip)

---
+++ src/DCHSBM/timing.jl
@@ -0,0 +1,45 @@
+using Statistics
+using FBDCompare: sumOfExteriorDegreesIntensityFunction, sampleSBM, sampleEdges
+
+Ω = sumOfExteriorDegreesIntensityFunction((x, α)->(x[1]+1)^(-α[x[2]]*x[2]), 3)
+
+t = @elapsed d,r = sampleEdges([1,1,2,2], Float64[1,1,1,1], Ω, α=Float64[.1, .3, .9], kmin=2, kmax=3)
+hypergraphsize = sum([k*sum(values(x)) for (k,x) in d])
+println("","A: $(hypergraphsize/t), $t")
+
+t = @elapsed d,r = sampleEdges(repeat(1:5, inner=10), ones(5*10), Ω, α=Float64[.1, .3, .9], kmin=2, kmax=3)
+hypergraphsize = sum([k*sum(values(x)) for (k,x) in d])
+println("","B: $(hypergraphsize/t), $t")
+
+t = @elapsed d,r = sampleEdges(repeat(1:5, inner=30), ones(5*30), Ω, α=Float64[.01, .03, .09], kmin=2, kmax=3)
+hypergraphsize = sum([k*sum(values(x)) for (k,x) in d])
+println("","C: $(hypergraphsize/t), $t")
+
+Z = vcat([fill(i, ceil(Integer, 100*proportion)) for (i,proportion) in enumerate([.4,.1,.03,.2,.2,.07])]...)
+γ = 1.63
+θ = ((γ+1) .* rand(length(Z))) .^ (-γ-1)
+Ω = FBDCompare.allOrNothingIntensityFunction((x, α) -> x[1] ? α[1] : α[2], 3)
+t = @elapsed d,r = sampleEdges(Z, θ, Ω, α=[.0001,.00001], kmin=2, kmax=3)
+hypergraphsize = sum([k*sum(values(x)) for (k,x) in d])
+println("","D: $(hypergraphsize/t), $t")
+
+#begin coppied from HypergraphModularity/test/runtests.jl
+n = 40 # modified to change n from 20
+Z = rand(1:5, n)
+ϑ = dropdims(ones(1,n) + rand(1,n), dims = 1)
+μ = mean(ϑ)
+kmax = 4
+
+scaling_factor = .01 # modified to add scaling factor
+ω(x, α) = scaling_factor*(x[1]+1)^(-α[x[2]]*x[2]) # modified to add scaling factor
+
+α = repeat([2.0], kmax)
+
+Ω = sumOfExteriorDegreesIntensityFunction(ω, kmax)
+t = @elapsed d, nodes = sampleEdges(Z, ϑ, Ω;α=α, kmax=kmax, kmin = 1) # modified
+#end coppied from HypergraphModularity/test/runtests.jl
+hypergraphsize = sum([k*sum(values(x)) for (k,x) in d])
+edges = sum([sum(values(x)) for (k,x) in d])
+possible_edges = sum([binomial(n,k) for k in 1:kmax])
+density = edges/possible_edges
+println("","E: $(hypergraphsize/t), $t, density=$density")
--- src/FBDCompare.jl
+++ src/FBDCompare.jl
@@ -4,12 +4,16 @@ using FBD
 
 export Kronecker, HyperPA
 
-include("hyper_pa.jl")
+#include("hyper_pa.jl")
 
-include("kronecker.jl")
+#include("kronecker.jl")
 
-include("er.jl")
+#include("er.jl")
 
-include("figure1.jl")
+#include("figure1.jl")
+
+include("DCHSBM/utils.jl")
+include("DCHSBM/omega.jl")
+include("DCHSBM/HSBM.jl")
 
 end
