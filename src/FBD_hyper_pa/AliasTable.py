84a5169277f329792d0a9245e4ee5906bc1d40b5
Lilith Hafner <Lilith.Hafner@gmail.com>
2022-06-20 14:37:17 -0400

    add alias table parsing

--- src/FBD_hyper_pa/AliasTable.py
+++ src/FBD_hyper_pa/AliasTable.py
@@ -2,9 +2,9 @@ import numpy as np
 import random
 
 class AliasTable:
-    def __init__(self, values, weigts):
+    def __init__(self, values, weights):
         self.values = values
-        self.accept = np.array(weigts, dtype=float) / sum(weigts) * len(values)
+        self.accept = np.array(weights, dtype=float) / sum(weights) * len(values)
         self.alias = np.zeros(len(values), dtype=int)
 
         small = []
@@ -36,6 +36,19 @@ class AliasTable:
             return self.values[idx]
         else:
             return self.values[self.alias[idx]]
+
+def parse_alias_table(filename):
+    values = []
+    weights = []
+    for i, line in enumerate(open(filename)):
+        if line == "0" or line == "":
+            continue
+        values.append(i+1)
+        weights.append(int(line))
+    return AliasTable(values, weights)
+# parse with:
+# at = AliasTable.parse_alias_table("../hyper_pa/simplex per node/DAWN-simplices-per-node-distribution.txt")
+# print([at.sample() for _ in range(100)])
 
 if __name__ == '__main__':
     values = [1, 7, 2, 9]
