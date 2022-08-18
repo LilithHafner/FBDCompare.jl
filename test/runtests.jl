02975a831d1b10ff8fc986dfd9068dd63e6df79b
Lilith Hafner <Lilith.Hafner@gmail.com>
2022-08-18 11:59:29 -0500

    add mock tests for scale > 0

--- test/runtests.jl
+++ test/runtests.jl
@@ -27,3 +27,25 @@ end
     FBDCompare.make_figure_0()
     FBDCompare.save_figures() # figures 1-6
 end
+
+@testset "mock tests" begin
+    @eval function FBDCompare.datapoint(_, target_size)
+        size = round(Int, target_size*(rand() + .5))
+        time = size * (rand() + .5) * 1e-8
+        size, time, :banana
+    end
+    @eval function FBDCompare.datapoint(_, target_size, target_density)
+        size = round(Int, target_size*(rand() + .5))
+        time = size * (rand() + .5) * 1e-8
+        density = target_density + randn()
+        size, time, density
+    end
+
+    # Note: missing figure 0
+    FBDCompare.SCALE[] = 0
+    FBDCompare.save_figures()
+    FBDCompare.SCALE[] = 1
+    FBDCompare.save_figures()
+    FBDCompare.SCALE[] = 2
+    FBDCompare.save_figures()
+end
